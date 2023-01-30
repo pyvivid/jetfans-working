76d373cf3c2706b4c7909c975fa93cc9f0b71793
Ubuntu <ubuntu@taj-01.us-west1-b.c.our-time-372214.internal>
2023-01-30 11:13:03 +0000

    test

--- providers.tf
+++ providers.tf
@@ -9,5 +9,5 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
-  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
 }
